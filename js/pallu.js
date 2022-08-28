44bbc502893f9b3afe2e1c9b2b9c1c9de3ceb93b
kamalaak <kamalstiries007@gmail.com>
2022-08-28 14:24:15 +0530

    solution page

---
+++ js/pallu.js
@@ -0,0 +1,9 @@
+let pallu = document.getElementById('cringekamalesh');
+
+function buttontoggel(){
+    if (pallu.classList.contains('hidden')) {
+        pallu.classList.remove('hidden')
+    }else{
+        pallu.classList.add('hidden')
+    }
+}
